27811f06f95d1589ade5b08949c3ffb230b5e07f
Kavya B M <Kavya.BM@valourtechnologies.com>
2026-04-10 17:54:25 +0530

    Backend (attendance + offer letter fix)

--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -1,7 +1,5 @@
 import express from "express";
 import multer from "multer";
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { protect } from "../middleware/authMiddleware.js";
 import {
   getEmployees,
@@ -11,22 +9,6 @@ import {
   updateEmployee,
   deleteEmployee,
   exportEmployeesToExcel,
-=======
-=======
-import { protect } from "../middleware/authMiddleware.js";
->>>>>>> 434a8b7 (final attendance report + offer letter)
-import {
-  getEmployees,
-  getEmployeeById,
-  getEmployeeByLookup,
-  addEmployee,
-  updateEmployee,
-  deleteEmployee,
-<<<<<<< HEAD
->>>>>>> da0db0d (backend project setup)
-=======
-  exportEmployeesToExcel,
->>>>>>> 434a8b7 (final attendance report + offer letter)
 } from "../controllers/employeeController.js";
 
 const router = express.Router();
@@ -40,20 +22,11 @@ const upload = multer({ storage });
 
 // Routes
 router.get("/", getEmployees);
-<<<<<<< HEAD
-<<<<<<< HEAD
 router.get("/lookup", getEmployeeByLookup);
 router.get("/:id", getEmployeeById);
+
+// ✅ Protect add employee
 router.post("/", protect, addEmployee);
-=======
-router.get("/:id", getEmployeeById);
-router.post("/", addEmployee);
->>>>>>> da0db0d (backend project setup)
-=======
-router.get("/lookup", getEmployeeByLookup);
-router.get("/:id", getEmployeeById);
-router.post("/", protect, addEmployee);
->>>>>>> 434a8b7 (final attendance report + offer letter)
 
 // ✅ Update employee with education & experience file support
 router.put(
@@ -66,14 +39,8 @@ router.put(
 );
 
 router.delete("/:id", deleteEmployee);
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 434a8b7 (final attendance report + offer letter)
+
+// ✅ Export
 router.get("/export/excel", exportEmployeesToExcel);
 
 export default router;
-=======
-
-export default router;
->>>>>>> da0db0d (backend project setup)
